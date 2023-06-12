53d4355906177185c2c3666be911aa8c87f8b0f8
ducloc <locdivine@gmail.com>
2023-06-12 14:15:42 +0700

    Hien thi doan chat gan day

--- app/src/main/java/com/lucky/chatappwithfirebase/MainActivity.java
+++ app/src/main/java/com/lucky/chatappwithfirebase/MainActivity.java
@@ -38,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId()==R.id.menu_chat){
+                if (item.getItemId() == R.id.menu_chat) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
                 }
-                if (item.getItemId()==R.id.menu_profile){
+                if (item.getItemId() == R.id.menu_profile) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, profileFragment).commit();
                 }
                 return true;
